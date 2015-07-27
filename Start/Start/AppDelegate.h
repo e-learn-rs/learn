eddd7600e0e3693334201181880dd91044680ff3
Robin-Mac <robinsinghrs07@gmail.com>
2015-07-28 00:35:30 +0530

    Initial Commit

---
+++ Start/Start/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Start
+//
+//  Created by Robin on 28/07/15.
+//  Copyright (c) 2015 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Start/Start/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Start
+//
+//  Created by Robin on 28/07/15.
+//  Copyright (c) 2015 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
