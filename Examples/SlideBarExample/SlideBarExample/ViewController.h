d770f55cfe4fa57366e4f956ac16ba14b4dc0875
Robin-Mac <robinsinghrs07@gmail.com>
2015-08-19 00:30:07 +0530

    Adding slidebar example

---
+++ Examples/SlideBarExample/SlideBarExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideBarExample
+//
+//  Created by Robin on 18/08/15.
+//  Copyright (c) 2015 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
